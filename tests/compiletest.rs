185db426090cc5a4e453db9da0ec10911301de6c
Andreas Fuchs <asf@boinkor.net>
2020-11-01 17:27:43 -0500

    Refactor tests & fix for new error format in 1.48
    The beta is failing, that's a good indicator that we should update the
    test cases.
    
    Also, make sure we don't run the nightly tests as part of the other
    test cases, and number them correctly with version numbers.

--- tests/compiletest.rs
+++ tests/compiletest.rs
@@ -1,17 +1,9 @@
-#[rustversion::any(nightly, beta)]
+#[rustversion::any(nightly)]
 #[test]
 fn compile_test_prerelease() {
     let t = trybuild::TestCases::new();
     t.pass("tests/run-pass/*.rs");
-    t.compile_fail("tests/compile-fail_1.46/*.rs");
-}
-
-#[rustversion::any(since(1.46))]
-#[test]
-fn compile_test_since_1_46() {
-    let t = trybuild::TestCases::new();
-    t.pass("tests/run-pass/*.rs");
-    t.compile_fail("tests/compile-fail_1.46/*.rs");
+    t.compile_fail("tests/compile-fail_nightly/*.rs");
 }
 
 #[rustversion::before(1.46)] // nightly+beta has changed the format of the macro backtrace hint.
@@ -20,4 +12,20 @@ fn compile_test_until_1_45() {
     let t = trybuild::TestCases::new();
     t.pass("tests/run-pass/*.rs");
     t.compile_fail("tests/compile-fail_1.45/*.rs");
+}
+
+#[rustversion::all(since(1.46), before(1.48))]
+#[test]
+fn compile_test_since_1_46() {
+    let t = trybuild::TestCases::new();
+    t.pass("tests/run-pass/*.rs");
+    t.compile_fail("tests/compile-fail_1.46/*.rs");
+}
+
+#[rustversion::all(since(1.48), not(nightly))]
+#[test]
+fn compile_test_stable_since_1_48() {
+    let t = trybuild::TestCases::new();
+    t.pass("tests/run-pass/*.rs");
+    t.compile_fail("tests/compile-fail_1.48/*.rs");
 }
